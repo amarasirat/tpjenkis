8a07ebf25426b106e0ceade093341b65c2d77aa3
amarasirat <amarasirat@gmail.com>
2025-12-02 13:32:49 +0100

    first commit

---
+++ dotnet-app.Tests/MathUtilsTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class MathUtilsTests
+{
+    [TestMethod]
+    public void TestFactorial5()
+    {
+        Assert.AreEqual(120, MathUtils.Factorial(5));
+    }
+
+    [TestMethod]
+    public void TestFactorial0()
+    {
+        Assert.AreEqual(1, MathUtils.Factorial(0)); // corrigé ici
+    }
+}
